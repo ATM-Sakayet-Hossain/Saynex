a68cb1912addf3a1493cc5a5538517cd6f0bddcc
ATM SAKAYET HOSSAIN <mdshakhawat434@gmail.com>
2025-06-13 16:06:33 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {dbConfig} from '../FirebaseConfig.js'
+import {app} from '../FirebaseConfig.js'
 
 
 createRoot(document.getElementById('root')).render(
