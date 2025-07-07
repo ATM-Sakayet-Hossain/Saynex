1646d8d86844d43c6240a973683121f8ba840eb4
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2025-07-07 21:24:31 +0600

    chatbox

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -19,9 +19,6 @@ const UserList = ({ data }) => {
       time: new Date().toLocaleTimeString([], {
         hour: "2-digit",
         minute: "2-digit",
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
       }),
   });
   };
