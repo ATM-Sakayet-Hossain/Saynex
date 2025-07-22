3dcca0134283dcd5c6fe08d2d862d0d12b95353a
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2025-07-22 23:36:26 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Layout from "./components/layout/Index";
 import ForgotPassword from "./pages/ForgotPassword"
-import ResetPassword from "./pages/ResetPassword"
 import Home from "./pages/Home";
 import Error from "./pages/Error";
-import Group from "./pages/Group";
-import Profile from "./pages/Profile";
-import Contacts from "./pages/Contacts";
 
 
 function App() {
@@ -22,9 +18,6 @@ function App() {
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/" element={<Layout/>} >
             <Route index element={<Home/>} />
-            <Route path="/group" element={<Group />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contacts" element={<Contacts />} />
           </Route>
           <Route path="*" element={<Error/>} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import ChatList from "../components/ChatList";
-import ChatBox from "../components/ChatBox";
-import { useSelector } from "react-redux";
 
 const Home = () => {
-  const activeFriend = useSelector((state) => state.activeFriend.friend);
   return (
     <>
       <div className="flex h-screen bg-gray-100 w-full">
-        <ChatList />
-        {activeFriend ? (
-          <ChatBox />
-        ) : (
-          <div className="w-full bg-slate-50 h-screen flex items-center justify-center">
-            <p className="text-secondary text-center">
-              Select a chat or start a new conversation
-            </p>
-          </div>
-        )}
+        <h2>Home</h2>
       </div>
     </>
   );
